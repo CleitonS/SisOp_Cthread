c754b13aada8e97df9c8633f7dd08a081348ace9
CleitonS <cleiton1302@gmail.com>
2018-04-10 19:45:48 -0300

    cidentify - não funciona :/
    Acho que não sei mais usar ponteiros. Não printa nada.

---
+++ cthread/src/cthread.c
@@ -0,0 +1,12 @@
+#include "../include/cdata.h"
+#include "../include/support.h"
+#include "../include/cthread.h"
+#include "../include/cthreadaux.h"
+
+
+
+int cidentify (char *name, int size){
+	name = "Bernardo Neuhaus Lignati - 230159 \nCleiton Souza Lima - 262511\nLucas Augusto - xxxx \n";
+	/*Falta a parte do tamanho...*/
+	return 0;
+}
