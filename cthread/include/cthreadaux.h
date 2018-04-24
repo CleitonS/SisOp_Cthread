49f326a6b299bd7c1a8ac9fced85c2f9235f8630
CleitonS <cleiton1302@gmail.com>
2018-04-24 00:44:03 -0300

    Criacao das funções para manipulação das filas: escalonador
    Funções:
    nextApto
    shiftNextApto
    shiftFilas
    printFila

--- cthread/include/cthreadaux.h
+++ cthread/include/cthreadaux.h
@@ -16,7 +16,7 @@ extern FILA2 aptos;
 extern FILA2 bloqueados;
 extern FILA2 aptos_sus;
 extern FILA2 bloqueados_sus;
-extern TCB_t execute;
+extern TCB_t *execute;
 extern csem_t semafaro;
 
 
@@ -31,7 +31,7 @@ int findInFila(int tid, FILA2 *fila){
     else{
         while (GetAtIteratorFila2(fila) != NULL){
             thread = GetAtIteratorFila2(fila);
-            if (thread -> tid == tid)
+            if (thread->tid == tid)
                 return 0;
             else
                 if (NextFila2(fila) != 0)
@@ -77,14 +77,142 @@ int insertInFila(FILA2 *fila, TCB_t *newThread){
 		AppendFila2(fila, (void *) newThread);
 		return 0;
 	}
-	else if (InsertAfterIteratorFila2(fila, newThread) != 0)
+	else {
+		if (LastFila2(fila) != 0)
+			return -1;
+		if (InsertAfterIteratorFila2(fila, newThread) != 0)
+			return -1;
+		else 
+			return 0;	
+	}	
+}
+
+
+
+
+/*-------------------------------------------------------------------
+Função:	Realiza a troca da thread que esta executando.
+		Pega o primeiro elemento da fila de aptos e coloca na 
+		variável execute.	
+Retorna 0 se a operação acorreu sem problemas.
+Retona !0 se ocorreu se erro.
+		
+		IMPORTANTE: Caso ocorra qualquer erro no processo, a thread 
+		que estava executando, continua executando...
+
+Erros:
+	-1 -> Falha ao posicionar o primeiro elemento da fila de aptos.
+	-2 -> Falha ao obter o nodo da fila.
+	-3 -> Falha ao excluir elemento da fila.
+-------------------------------------------------------------------*/
+int nextApto(){
+	TCB_t *OldNode;	
+
+	if (FirstFila2(&aptos) != 0)
 		return -1;
-	else 
-		return 0;	
+	else{
+		OldNode = execute;
+		execute = GetAtIteratorFila2(&aptos); /*Testar...*/
+		if (execute == NULL){
+			execute = OldNode;
+			return -2;
+		}
+		else{
+			if (DeleteAtIteratorFila2(&aptos) != 0){
+				execute = OldNode;
+				return -3;
+			}
+			else			
+				return 0;	
+		}
+	}
+}
+
+
+/*-------------------------------------------------------------------
+Função:	Insere a thread que esta executando na fila passada por 
+parametro e coloca o primeiro elemento da fila de aptos para executar.
+
+Retorna 0 se a operação acorreu sem problemas.
+Retona !0 se ocorreu se erro.
+
+ERROS:
+	-1 -> Erro ao pegar o primeiro elemento da fila de aptos e 
+			colocá-lo a executar.
+	-2 -> Erro ao inserir a thread que estava executando na
+			fila passada por parâmetro.
+-------------------------------------------------------------------*/
+int shiftNextApto(FILA2 *fila){
+	TCB_t *OldNode;
+	OldNode = execute;
+	
+	if (nextApto() != 0)
+		return -1;
+	else{
+		if (insertInFila(fila, OldNode) != 0 )
+			return -2;
+		else 
+			return 0;
+	}
+}
+
+
+
+
+/*-------------------------------------------------------------------
+Função:	Recebe duas filas (FilaIn e FilaOut) e o identificador da 
+		thread tid.
+		A operação da função é:
+		Procurar a thread correspondente ao tid na filaOut;
+		remover o nodo da filaOut;
+		Inserir o nodo no final da filaIn.
+		
+
+Retorna 0 se a operação acorreu sem problemas.
+Retona !0 se ocorreu se erro.
+
+ERROS:
+	-1 -> Tid não encontrado			
+	-2 -> Erro ao obter o ponteiro para a thread 
+	-3 -> Erro ao excluir nodo
+	-4 -> Erro ao inserir o nodo na filaIn			
+-------------------------------------------------------------------*/
+int shiftFilas(FILA2 *filaIn, FILA2 *filaOut, int tidOut){
+	TCB_t *nodeOut;
+	//TCB_t BackNodeOut;
+	
+	if (findInFila(tidOut, filaOut) != 0) //posiciona o iterador da fila no elemento
+		return -1;
+	nodeOut = GetAtIteratorFila2(filaOut);
+	if (nodeOut == NULL)
+		return -2;
+	if(DeleteAtIteratorFila2(filaOut) != 0) 
+		return -3;
+	if (insertInFila(filaIn, nodeOut) != 0)
+		return -4;
+	return 0;	
+}
+
+
+
+void printFila(FILA2 *fila){
+    TCB_t *thread = NULL;
+	if (FirstFila2(fila) != 0)
+        printf("ERRO - fila vazia/n");
+    else{
+        while (GetAtIteratorFila2(fila) != NULL){
+			thread = GetAtIteratorFila2(fila);
+			printf("tid: %d, ", thread->tid);
+			NextFila2(fila);			
+        }
+    }	
 	
 	
 	
 }
+
+
+
 
 
 #endif
