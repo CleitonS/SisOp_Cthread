08d6e79317bfa1734782b1abe32d9d977caa9229
CleitonS <cleiton1302@gmail.com>
2018-04-26 19:24:51 -0300

    CIdentify básica...

--- cthread/src/cthread.c
+++ cthread/src/cthread.c
@@ -16,14 +16,11 @@ FILA2 bloqueados_sus;
 TCB_t *execute;
 csem_t semafaro; /*Obs: Na especificacao, o nome da variavel esta descrita com acento: "semï¿½faro". */
 
+
 int cidentify (char *name, int size){
-	/*Nï¿½o esta funcionando!!! */
 	name = "Bernardo Neuhaus Lignati - 230159 \nCleiton Souza Lima - 262511\nLucas Augusto - 265038 \n";
-    char * nomes;
-	/*Não esta funcionando!!! */
-	//name = "Bernardo Neuhaus Lignati - 230159 \nCleiton Souza Lima - 262511\nLucas Augusto - xxxx \n";
-	/*Falta a parte do tamanho...*/
-    nomes = calloc(size, sizeof(char));
-    nomes = name;
-	return 0;
+	if (sizeof(name) > size) 
+		return -1;
+	else 
+		return 0;
 }
