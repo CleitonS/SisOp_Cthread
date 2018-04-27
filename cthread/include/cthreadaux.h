dc20df13bf2afb71e1de4452353d67d974e26298
CleitonS <cleiton1302@gmail.com>
2018-04-26 22:11:53 -0300

    #15  Função de termino de thread.

--- cthread/include/cthreadaux.h
+++ cthread/include/cthreadaux.h
@@ -18,14 +18,14 @@ extern FILA2 aptos_sus;
 extern FILA2 bloqueados_sus;
 extern TCB_t *execute;
 extern csem_t semafaro;
-
+extern TCB_t threadMain;
 
 /*-------------------------------------------------------------------
 Função:	Retorna 0 se o tid passado se encontra na fila passado como 
 		parâmetro e !0 se não se encontra.
 -------------------------------------------------------------------*/
 int findInFila(int tid, FILA2 *fila){
-    TCB_t *thread = NULL;
+    TCB_t *thread = (TCB_t *) malloc(sizeof(TCB_t));
 	if (FirstFila2(fila) != 0)
         return -1;
     else{
@@ -46,17 +46,32 @@ int findInFila(int tid, FILA2 *fila){
 /*-------------------------------------------------------------------
 Função:	Retorna !0 se o tid passado já esta sendo esperado por outra
 		thread da fila passada. Do contrário, retorna 0.
+		
+		Caso o retorno passado seja !0, esse número é o tid da 
+		thread que estava esperando o tid passado por parâmetro.
+		Exceto se for a thread main, nesse caso retorna -1.
+		
+		Exemplo:
+		Quero saber se alguma thead esta esperando o tid = 5. 
+		Se o retorno for igual a 2, significa que a thread 2 
+		esta esperando a thread 5 terminar.
+		Se o retorno for -1, significa que a theadMain que esta 
+		esperando a thread 5
 -------------------------------------------------------------------*/
 int findOtherJoin(int tid, FILA2 *fila){
 
-    TCB_t *thread = NULL;
+    TCB_t *thread = (TCB_t *) malloc(sizeof(TCB_t));
 	if (FirstFila2(fila) != 0)
         return 0;
     else{
         while (GetAtIteratorFila2(fila) != NULL){
             thread = GetAtIteratorFila2(fila);
-            if (thread -> waintingJoin == tid)
-                return -1;
+            if (thread -> waintingJoin == tid){
+				if (thread -> tid == 0)					
+					return -1;
+				else 
+					return (thread -> tid);
+			}
             else
                 if (NextFila2(fila) != 0)
                     return 0;
@@ -113,7 +128,7 @@ Erros:
 	-3 -> Falha ao excluir elemento da fila.
 -------------------------------------------------------------------*/
 int nextApto(){
-	TCB_t *OldNode;	
+	TCB_t *OldNode = (TCB_t *) malloc(sizeof(TCB_t));	
 
 	if (FirstFila2(&aptos) != 0)
 		return -1;
@@ -183,7 +198,7 @@ ERROS:
 	-4 -> Erro ao inserir o nodo na filaIn			
 -------------------------------------------------------------------*/
 int shiftFilas(FILA2 *filaIn, FILA2 *filaOut, int tidOut){
-	TCB_t *nodeOut;
+	TCB_t *nodeOut = (TCB_t *) malloc(sizeof(TCB_t));
 	//TCB_t BackNodeOut;
 	
 	if (findInFila(tidOut, filaOut) != 0) //posiciona o iterador da fila no elemento
@@ -200,22 +215,68 @@ int shiftFilas(FILA2 *filaIn, FILA2 *filaOut, int tidOut){
 
 
 
+
+
+
+/*-------------------------------------------------------------------
+Função:	Finaliza a thread que está executando e busca se alguma outra
+		estava na fila de bloqueados aguardando seu término(cjoin).
+		Além de finalizar a thread, também coloca o próximo elemento
+		da fila de aptos para executar e retira de bloqueados a thread 
+		que estava aguardando e a coloca em aptos.
+
+Retorna 0 se a operação acorreu sem problemas.
+Retona !0 se ocorreu se erro.
+
+ERROS:
+	-1 -> Não encontrou a thead que a função findOutherJoin acusou 
+		  estar esperando pelo término.
+		  Erro na findOtherJoin ou na findInFila.
+			
+	-2 -> TID diferentes, erro no processo.
+	
+	-3 -> Erro ao chamar próxima thread a executar.
+			
+
+-------------------------------------------------------------------*/
+int finishThread(){
+	TCB_t *node = (TCB_t *) malloc(sizeof(TCB_t));
+	int tidWaiting;
+	
+	tidWaiting =  findOtherJoin(execute->tid, &bloqueados);
+	if (tidWaiting != 0){
+		if (tidWaiting == -1)
+			tidWaiting = 0;
+		
+		if (findInFila(tidWaiting, &bloqueados) != 0)
+			return -1;
+		node = GetAtIteratorFila2(&bloqueados);
+		if (node -> waintingJoin != execute->tid)
+			return -2;
+		else{
+			node -> waintingJoin = -1;
+			shiftFilas(&aptos, &bloqueados, node->tid);
+		}							
+	}
+	if (nextApto() != 0)
+		return -3;
+	else 
+		return 0;	
+}
+
+
 void printFila(FILA2 *fila){
-    TCB_t *thread = NULL;
+    TCB_t *thread = (TCB_t *) malloc(sizeof(TCB_t));
 	if (FirstFila2(fila) != 0)
-        printf("ERRO - fila vazia/n");
+        printf("ERRO - fila vazia\n");
     else{
         while (GetAtIteratorFila2(fila) != NULL){
 			thread = GetAtIteratorFila2(fila);
 			printf("tid: %d, ", thread->tid);
 			NextFila2(fila);			
         }
-    }	
-		
+    }		
 }
-
-
-
 
 
 #endif
