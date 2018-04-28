7067a9beaf369626736e7c64995356179bddb331
Lucas Augusto <lucas.tansini@hotmail.com>
2018-04-28 16:51:37 -0300

    Implementação da checkMain
    Toda vez que chamar as funções, e for a primeira vez, iniciamos as filas e main.

--- cthread/src/cthread.c
+++ cthread/src/cthread.c
@@ -17,6 +17,33 @@ TCB_t *execute;
 csem_t semafaro; /*Obs: Na especificacao, o nome da variavel esta descrita com acento: "sem�faro". */
 TCB_t threadMain;
 
+int init = 0;
+
+
+/*Função que testa se a Main está criada. Se não estiver, cria e inicializa ela.
+Retorna zero se tudo estiver certo, e negativo caso contrário.*/
+
+int checkMainThread(){
+
+	if(init != 0){
+		//Já inicializamos com a init
+		return 0; 
+	}else{
+		if(!initLib()){
+			printf("Thread Main nao estava iniciada e foi criada junto com as filas.\n");
+			init = 1;
+			return 0;
+		}else{
+			printf("Erro na execucao de initLib.\n");
+			return -1;
+		}
+	}
+
+}
+
+
+
+
 int cidentify (char *name, int size){
 	name = "Bernardo Neuhaus Lignati - 230159 \nCleiton Souza Lima - 262511\nLucas Augusto - 265038 \n";
 	if (sizeof(name) > size) 
